12d0a5beb372d86b75acfab6ff22bdcb90094109
Sagar Chamling <sagarchamling@lftechnology.com>
2018-03-26 00:16:06 +0545

    Include source mapping and environment in configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,8 +3,10 @@ const merge = require('webpack-merge');
 
 const common = require('./webpack.common.js');
 
+const NODE_ENV = 'development';
+
 module.exports = merge(common, {
-  mode: 'development',
+  mode: NODE_ENV,
 
   devtool: 'inline-source-map',
 
@@ -13,5 +15,13 @@ module.exports = merge(common, {
     contentBase: './dist'
   },
 
-  plugins: [new webpack.NamedModulesPlugin(), new webpack.HotModuleReplacementPlugin()]
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+
+    new webpack.HotModuleReplacementPlugin(),
+
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': NODE_ENV
+    })
+  ]
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,21 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+
+  devtool: 'source-map',
+
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true
+    }),
+
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': 'production'
+    })
+  ]
 });

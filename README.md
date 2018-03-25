e950b840185d8cd80b2e85cee6fd83123685a846
Sagar Chamling <sagarchamling@lftechnology.com>
2018-03-25 19:34:45 +0545

    Migrate webpack 3 to webpack 4 and reconfigure app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# es2015-way
+# webpack-starter
 
 <a href="https://travis-ci.org/cham11ng/webpack-starter">
 <img src="https://travis-ci.org/cham11ng/webpack-starter.svg?branch=master" alt="Build Status">
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,54 @@
-let webpack = require('webpack');
-let ExtractTextPlugin = require('extract-text-webpack-plugin');
-let inProduction = (process.env.NODE_ENV === 'production');
+var webpack = require('webpack');
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+  mode: process.env.NODE_ENV,
+
   entry: {
-    app: [
-      './src/main.js',
-      './src/main.scss'
-    ]
+    app: ['./src/index.js', './src/app.scss']
   },
+
   output: {
     path: __dirname + '/dist',
     filename: '[name].js'
   },
+
   devtool: 'source-map',
+
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/,
+        exclude: /node_modules/,
         use: ExtractTextPlugin.extract({
           use: ['css-loader', 'sass-loader'],
           fallback: 'style-loader'
         })
-      }, 
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader'
       }
     ]
   },
+
   plugins: [
     new ExtractTextPlugin('[name].css'),
+
     new webpack.LoaderOptionsPlugin({
-      minimize: inProduction
+      minimize: process.env.NODE_ENV === 'production'
+    }),
+
+    new HTMLWebpackPlugin({
+      title: 'Webpack Starter',
+      filename: 'index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: false,
+        removeAttributeQuotes: true
+      }
     })
   ]
 };
-
-if (inProduction) {
-  module.exports.plugins.push(
-    new webpack.optimize.UglifyJsPlugin()
-  );
-}
